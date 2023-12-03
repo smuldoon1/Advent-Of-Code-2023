8fe9424d90375327c232a386445d9de2b6d2d3f8
seanmul10 <36199832+smuldoon1@users.noreply.github.com>
2023-12-03 11:02:12 +0000

    Day 3

--- AdventOfCode/Day3/Program.cs
+++ AdventOfCode/Day3/Program.cs
@@ -1,74 +1,83 @@
 ﻿// Part 1 - 27m 22s
+// Part 2 - 29m 45s
 
 var input = File.ReadAllLines(@"F:\Advent-Of-Code-2023\AdventOfCode\Day3\input.txt");
 
-input = AddBorder();
+var numbers = new List<NumberSpan>();
+for (int i = 0; i < input.Length; i++)
+{
+    for (int j = 0; j < input.Length; j++)
+    {
+        if (char.IsDigit(input[i][j]))
+        {
+            var str = new string(input[i][j..].TakeWhile(x => int.TryParse(x.ToString(), out _)).ToArray());
+            numbers.Add(new NumberSpan
+            {
+                Row = i,
+                Column = j,
+                Value = int.Parse(str),
+                Length = str.Length
+            });
+            j += str.Length;
+        }
+    }
+}
 
-var total1 = 0;
+var partNumbers = new List<NumberSpan>();
 for (int i = 0; i < input.Length; i++)
 {
     for (int j = 0; j < input[i].Length; j++)
     {
-        if (int.TryParse(input[i][j].ToString(), out _))
+        var c = input[i][j];
+        if (IsSymbol(c))
         {
-            var number = new string(input[i][j..].TakeWhile(x => int.TryParse(x.ToString(), out _)).ToArray());
-
-            total1 += AddPartNumber(number, i, j);
-
-            j += number.Length - 1;
+            partNumbers.AddRange(numbers.Where(x =>
+                x.Row <= i + 1 &&
+                x.Row >= i - 1 &&
+                x.Column <= j + 1 &&
+                x.Column + x.Length >= j
+            ));
         }
     }
 }
-Console.WriteLine($"Part 1: {total1}");
+Console.WriteLine($"Part 1: {partNumbers.Distinct().Sum(x => x.Value)}");
 
-int AddPartNumber(string str, int row, int column)
+var total = 0;
+for (int i = 0; i < input.Length; i++)
 {
-    var number = int.Parse(str);
-
-    foreach (var c in input[row - 1].Substring(column - 1, str.Length + 2))
+    for (int j = 0; j < input[i].Length; j++)
     {
-        if (IsSymbol(c))
+        var c = input[i][j];
+        if (c == '*')
         {
-            return number;
+            var gearNumbers = numbers.Where(x =>
+                x.Row <= i + 1 &&
+                x.Row >= i - 1 &&
+                x.Column <= j + 1 &&
+                x.Column + x.Length >= j
+            ).ToArray();
+
+            if (gearNumbers.Count() == 2)
+            {
+                total += gearNumbers[0].Value * gearNumbers[1].Value;
+            }
         }
     }
-
-    foreach (var c in input[row + 1].Substring(column - 1, str.Length + 2))
-    {
-        if (IsSymbol(c))
-        {
-            return number;
-        }
-    }
-
-    if (IsSymbol(input[row][column - 1]))
-    {
-        return number;
-    }
-
-    if (IsSymbol(input[row][column + str.Length]))
-    {
-        return number;
-    }
-
-    return 0;
 }
+Console.WriteLine($"Part 2: {total}");
 
-bool IsSymbol(char c)
+static bool IsSymbol(char c)
 {
     return c != '.' && !char.IsLetterOrDigit(c);
 }
 
-string[] AddBorder()
+struct NumberSpan
 {
-    var newInput = new List<string>();
-    foreach (var line in input)
-    {
-        newInput.Add($".{line}.");
-    }
-    var topBottom = new string(Enumerable.Repeat('.', input[0].Length + 2).ToArray());
-    newInput.Insert(0, topBottom);
-    newInput.Add(topBottom);
+    public int Value { get; set; }
 
-    return newInput.ToArray();
+    public int Row { get; set; }
+
+    public int Column { get; set; }
+
+    public int Length { get; set; }
 }
